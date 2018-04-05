2ef038d9de39bb7250a4db59d1a7b95e7b50a2c0
gabor-szeles <szelesgbr@gmail.com>
2018-04-05 13:29:50 +0200

    created funfact entity and repository

--- src/main/java/org/funfactorium/funfacts/FunFact.java
+++ src/main/java/org/funfactorium/funfacts/FunFact.java
@@ -1,4 +1,84 @@
 package org.funfactorium.funfacts;
 
+import org.funfactorium.funfacts.topics.Topic;
+import org.funfactorium.user.User;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class FunFact {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String title;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    @ManyToOne
+    private User author;
+
+    @ManyToMany
+    private Set<Topic> topicSet = new HashSet<>();
+
+    private double rating;
+
+    public FunFact() {
+    }
+
+    public FunFact(String title, String description, User author, Topic topic, double rating) {
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.topicSet.add(topic);
+        this.rating = rating;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Set<Topic> getTopic() {
+        return topicSet;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
--- src/main/java/org/funfactorium/funfacts/FunFactRepository.java
+++ src/main/java/org/funfactorium/funfacts/FunFactRepository.java
@@ -1,4 +1,8 @@
 package org.funfactorium.funfacts;
 
-public interface FunFactRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FunFactRepository extends JpaRepository<FunFact, Long> {
 }
