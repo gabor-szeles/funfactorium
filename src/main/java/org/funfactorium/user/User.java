ac11a0d51ecc4096387ce399973d223f1c9fc262
gabor-szeles <szelesgbr@gmail.com>
2018-04-05 13:30:11 +0200

    created user entity and repository

--- src/main/java/org/funfactorium/user/User.java
+++ src/main/java/org/funfactorium/user/User.java
@@ -1,4 +1,71 @@
 package org.funfactorium.user;
 
+import org.funfactorium.funfacts.FunFact;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String nickName;
+
+    private String password;
+
+    private String email;
+
+    @OneToMany(mappedBy = "author")
+    private Set<FunFact> authoredFunFacts = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String nickName, String password, String email) {
+        this.nickName = nickName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public Set<FunFact> getAuthoredFunFacts() {
+        return authoredFunFacts;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
