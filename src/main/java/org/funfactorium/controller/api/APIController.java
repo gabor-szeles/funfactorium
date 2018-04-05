944d9586652041232318373525c1df600b5715dc
gabor-szeles <szelesgbr@gmail.com>
2018-04-05 17:18:52 +0200

    added filter method

--- src/main/java/org/funfactorium/controller/api/APIController.java
+++ src/main/java/org/funfactorium/controller/api/APIController.java
@@ -1,4 +1,24 @@
 package org.funfactorium.controller.api;
 
+import org.funfactorium.funfacts.FunFactService;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class APIController {
+
+    @Autowired
+    FunFactService funFactService;
+
+    @PostMapping(value = "/api/filter", consumes = "application/json")
+    public ResponseEntity renderFilteredIndex(@RequestBody String id) {
+        long topicId = Long.parseLong(id.split("=")[1]);
+        List<Map<String, String>> params = funFactService.searchByTopic(topicId);
+        return ResponseEntity.ok(params);
+    }
 }
