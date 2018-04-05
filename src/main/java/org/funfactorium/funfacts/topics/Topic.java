7a2a0ab5f54d5f4274bcc08eeb3bc2a542305263
gabor-szeles <szelesgbr@gmail.com>
2018-04-05 13:29:30 +0200

    created topics entity and repository

--- src/main/java/org/funfactorium/funfacts/topics/Topic.java
+++ src/main/java/org/funfactorium/funfacts/topics/Topic.java
@@ -1,4 +1,50 @@
 package org.funfactorium.funfacts.topics;
 
+import org.funfactorium.funfacts.FunFact;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Topic {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+
+    @ManyToMany
+    private Set<FunFact> funFactSet = new HashSet<>();
+
+    public Topic() {
+    }
+
+    public Topic(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Set<FunFact> getFunFacts() {
+        return funFactSet;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/org/funfactorium/funfacts/topics/TopicRepository.java
+++ src/main/java/org/funfactorium/funfacts/topics/TopicRepository.java
@@ -1,4 +1,8 @@
 package org.funfactorium.funfacts.topics;
 
-public interface TopicRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicRepository extends JpaRepository<Topic, Long> {
 }
