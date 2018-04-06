4848c2038299189a7423ded8a6fe27266c66003f
gabor-szeles <szelesgbr@gmail.com>
2018-04-06 13:54:23 +0200

    changed autowiring to injection with constructor in topicservice, tests corrected

--- src/test/java/org/funfactorium/funfacts/topics/TopicServiceTest.java
+++ src/test/java/org/funfactorium/funfacts/topics/TopicServiceTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 class TopicServiceTest {
 
+    private static TopicService testService;
     private static TopicRepository mockRepository;
     private static List<Topic> mockTopicList;
 
@@ -25,19 +26,20 @@ class TopicServiceTest {
         Topic mockTopic2 = mock(Topic.class);
         mockTopicList.add(mockTopic1);
         mockTopicList.add(mockTopic2);
+        testService = new TopicService(mockRepository);
     }
 
     @Test
     public void testFindAllWorksCorrectly() {
         when(mockRepository.findAll()).thenReturn(mockTopicList);
-        List<Topic> testList = mockRepository.findAll();
+        List<Topic> testList = testService.allTopics();
         assertEquals(2, testList.size());
     }
 
     @Test
     public void testFindAllCanReturnEmptyList() {
         when(mockRepository.findAll()).thenReturn(new ArrayList<>());
-        List<Topic> testList = mockRepository.findAll();
+        List<Topic> testList = testService.allTopics();
         assertEquals(0, testList.size());
     }
 
