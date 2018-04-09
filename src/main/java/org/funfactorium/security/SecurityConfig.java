70ada89bd156cfdae08655882497f3a596f6a3f0
Szűcs Tibor <job.szucstibor@gmail.com>
2018-04-09 13:52:10 +0200

    login 405 error resolved

--- src/main/java/org/funfactorium/security/SecurityConfig.java
+++ src/main/java/org/funfactorium/security/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/login*", "/api/**", "/api-docs").anonymous()
+                .antMatchers("/", "/login*", "/api/**", "/api-docs").anonymous()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login")
@@ -29,6 +29,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
+                .permitAll()
+                .and().formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
                 .permitAll()
                 .and()
                 .csrf().disable();
