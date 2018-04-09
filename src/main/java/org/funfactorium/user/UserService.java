6ff49b63d894920d008baffc786f88e9cb3a0cce
gabor-szeles <szelesgbr@gmail.com>
2018-04-09 14:35:00 +0200

    added necessary methods for authentication

--- src/main/java/org/funfactorium/user/UserService.java
+++ src/main/java/org/funfactorium/user/UserService.java
@@ -1,4 +1,51 @@
 package org.funfactorium.user;
 
-public class UserService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService{
+
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUserName(username);
+
+        if (user == null) {
+            String message = "Username not found: " + username;
+            log.info(message);
+            throw new UsernameNotFoundException(message);
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("user"));
+
+
+        return new org.springframework.security.core.userdetails.User(
+                username,
+                user.getPassword(),
+                true,
+                true, //Account Not Expired
+                true, //Credentials Not Expired
+                true,//Account Not Locked
+                authorities
+        ) {
+        };
+
+    }
 }
