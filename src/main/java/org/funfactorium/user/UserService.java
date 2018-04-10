9d8d5279591d18db89981711399fad2de43eb2a7
Szűcs Tibor <job.szucstibor@gmail.com>
2018-04-10 13:11:26 +0200

    Added a new method (register) to save users in database

--- src/main/java/org/funfactorium/user/UserService.java
+++ src/main/java/org/funfactorium/user/UserService.java
@@ -1,5 +1,7 @@
 package org.funfactorium.user;
 
+import org.funfactorium.security.email.EmailValidator;
+import org.funfactorium.security.password.PasswordMatchesValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +11,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserService implements UserDetailsService{
@@ -48,4 +53,14 @@ public class UserService implements UserDetailsService{
         };
 
     }
+
+    public void register(Map<String, String> requestParams){
+        String username = requestParams.get("username");
+        String password = requestParams.get("password");
+        String matchingPassword = requestParams.get("matchingpassword");
+        String email = requestParams.get("email");
+        User user = new User(username, password, email, matchingPassword);
+        userRepository.save(user);
+    }
+
 }
