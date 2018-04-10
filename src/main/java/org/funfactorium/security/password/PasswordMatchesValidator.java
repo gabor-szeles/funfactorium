516d86b679609ebcc75703d9affecd74c63b699d
Szűcs Tibor <job.szucstibor@gmail.com>
2018-04-10 13:08:46 +0200

    Password validator created with custom annotation

--- src/main/java/org/funfactorium/security/password/PasswordMatchesValidator.java
+++ src/main/java/org/funfactorium/security/password/PasswordMatchesValidator.java
@@ -1,4 +1,20 @@
 package org.funfactorium.security.password;
 
-public class PasswordMatchesValidator {
+
+import org.funfactorium.user.User;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext context) {
+        User user = (User) obj;
+        return user.getPassword().equals(user.getMatchingPassword());
+    }
 }
